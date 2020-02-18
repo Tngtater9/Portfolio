ac973a9701685f5c61a5a0ea615b0ef9e28481c1
Jamie Tater <jtater1@gmail.com>
2020-02-17 23:08:39 -0500

    changed icons

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 function handleAbout () {
-
+    $('.nav-about').click(function (){
+        $('h2').text('About');
+        $('.about').show();
+        $('.projects').hide();
+        $('.contacts').hide();
+    });
 }
 
 function handleProjects() {
-
+    $('.nav-projects').click(function (){
+        $('h2').text('Projects');
+        $('.projects').show();
+        $('.contacts').hide();
+        $('.about').hide();
+    });
 }
 
 function handleContacts() {
-
+    $('.nav-about').click(function (){
+        $('h2').text('Contacts');
+        $('.contacts').show();
+        $('.about').hide();
+        $('.projects').hide();
+    });
 }
 
 
@@ -16,3 +31,5 @@ function handlePortfolio () {
     handleProjects();
     handleContacts();
 }
+
+$(handlePortfolio());
