a486c8d8527bf4cf01409dc247f69f3dad099c8b
Jamie Tater <jtater1@gmail.com>
2020-02-19 00:55:20 -0500

    final draft

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-function handleAbout () {
-    $('.nav-about').click(function (){
-        $('h2').text('About');
-        $('.about').show();
-        $('.projects').hide();
-        $('.contacts').hide();
+function showMenu () {
+    if(screen.width > 700){
+        $('.nav').show();
+    }
+    $('.menu-icon').click(function(){
+        $('.nav').show();
+        $('nav').focus();
+    });
+    $('nav').blur(function(){
+        console.log("blur works");
+        hideMenu();
     });
 }
+function hideMenuIcon () {
+    if(screen.width > 700){
+        $('.menu-icon').hide();
+    }
+}
+function hideMenu () {
+        $('.nav').hide();
+}
+function handleAbout () {
+    $('.nav-about').click(showAbout);
+}
+function showAbout () {
 
+    $('h2').text('About');
+    $('.about').show();
+    $('.projects').hide();
+    $('.contacts').hide();
+}
 function handleProjects() {
     $('.nav-projects').click(function (){
         $('h2').text('Projects');
@@ -17,7 +39,7 @@ function handleProjects() {
 }
 
 function handleContacts() {
-    $('.nav-about').click(function (){
+    $('.nav-contacts').click(function (){
         $('h2').text('Contacts');
         $('.contacts').show();
         $('.about').hide();
@@ -27,6 +49,10 @@ function handleContacts() {
 
 
 function handlePortfolio () {
+    hideMenu();
+    hideMenuIcon();
+    showMenu();
+    showAbout();
     handleAbout();
     handleProjects();
     handleContacts();
